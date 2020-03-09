cf3b68b00e18eae7b813f5c2d5125b6ae7ca8093
Pedro Machado <pedrohmf2610@hotmail.com>
2020-03-09 12:13:15 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/basis/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/basis/cursomc/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.basis.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.basis.cursomc.domain.Categoria;
+import com.basis.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-	
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
 }
